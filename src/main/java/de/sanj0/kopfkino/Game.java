298068330875545909996bd1ec88b3816fe35b02
M <geotag-caddies8x@icloud.com>
2021-09-21 19:09:54 +0200

    add core of engine

---
+++ src/main/java/de/sanj0/kopfkino/Game.java
@@ -0,0 +1,130 @@
+/*
+ *    Copyright 2021 ***REMOVED*** ***REMOVED***
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ *
+ */
+
+package de.sanj0.kopfkino;
+
+import de.sanj0.kopfkino.engine.FixedUpdateLoop;
+import de.sanj0.kopfkino.engine.RenderLoop;
+import de.sanj0.kopfkino.ui.KopfkinoWindow;
+
+import java.awt.*;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+public class Game {
+
+    private static Game instance = null;
+
+    private int resolutionW;
+    private int resolutionH;
+    private long fixedUpdatePeriod;
+    private String name;
+    private Color backgroundColor;
+
+    private KopfkinoWindow window;
+    private ScheduledExecutorService executorService;
+
+    private Game(final int resolutionW, final int resolutionH, final String name, final Color backgroundColor) {
+        this.resolutionW = resolutionW;
+        this.resolutionH = resolutionH;
+        this.name = name;
+        this.backgroundColor = backgroundColor;
+    }
+
+    public static void init(final int resolutionW, final int resolutionH,
+                            final String name) {
+        if (instance != null) {
+            System.err.println("game already initialised");
+            System.exit(1);
+        }
+        instance = new Game(resolutionW, resolutionH, name, Color.BLACK);
+    }
+
+    public static void start(final long fixedUpdatePeriod, final int cappedFPS) {
+        instance.fixedUpdatePeriod = fixedUpdatePeriod;
+        instance.window = new KopfkinoWindow(instance.resolutionW, instance.resolutionH, instance.name);
+        instance.window.setVisible(true);
+
+        instance.executorService = Executors.newScheduledThreadPool(2);
+        instance.executorService.scheduleAtFixedRate(new FixedUpdateLoop(), instance.fixedUpdatePeriod, instance.fixedUpdatePeriod, TimeUnit.MILLISECONDS);
+        instance.executorService.scheduleAtFixedRate(new RenderLoop(), 0, cappedFPS, TimeUnit.MILLISECONDS);
+    }
+
+    public static void exit() {
+        instance.executorService.shutdownNow();
+        System.exit(0);
+    }
+
+    public static Game getInstance() {
+        return instance;
+    }
+
+    /**
+     * Gets {@link #resolutionW}.
+     *
+     * @return the value of {@link #resolutionW}
+     */
+    public int getResolutionW() {
+        return resolutionW;
+    }
+
+    /**
+     * Gets {@link #resolutionH}.
+     *
+     * @return the value of {@link #resolutionH}
+     */
+    public int getResolutionH() {
+        return resolutionH;
+    }
+
+    /**
+     * Gets {@link #fixedUpdatePeriod}.
+     *
+     * @return the value of {@link #fixedUpdatePeriod}
+     */
+    public long getFixedUpdatePeriod() {
+        return fixedUpdatePeriod;
+    }
+
+    /**
+     * Gets {@link #name}.
+     *
+     * @return the value of {@link #name}
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Gets {@link #backgroundColor}.
+     *
+     * @return the value of {@link #backgroundColor}
+     */
+    public Color getBackgroundColor() {
+        return backgroundColor;
+    }
+
+    /**
+     * Gets {@link #window}.
+     *
+     * @return the value of {@link #window}
+     */
+    public KopfkinoWindow getWindow() {
+        return window;
+    }
+}
---
+++ src/main/java/de/sanj0/kopfkino/ui/KopfkinoWindow.java
@@ -0,0 +1,49 @@
+/*
+ *    Copyright 2021 ***REMOVED*** ***REMOVED***
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ *
+ */
+
+package de.sanj0.kopfkino.ui;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * The window the game is rendered in.
+ */
+public class KopfkinoWindow extends JFrame {
+
+    private KopfkinoCanvas canvas;
+
+    public KopfkinoWindow(final int w, final int h, final String gameName) throws HeadlessException {
+        super(gameName + " (kopfkino engine)");
+
+        setLayout(null);
+        setSize(w, h);
+        setLocationRelativeTo(null);
+
+        canvas = new KopfkinoCanvas(w, h);
+        add(canvas);
+    }
+
+    /**
+     * Gets {@link #canvas}.
+     *
+     * @return the value of {@link #canvas}
+     */
+    public KopfkinoCanvas getCanvas() {
+        return canvas;
+    }
+}
