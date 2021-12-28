0e4cd19f390435fed6e506c63e3a6e8c02c5d3d0
M <geotag-caddies8x@icloud.com>
2021-12-28 22:40:39 +0100

    implement static serialization

---
+++ src/main/java/de/sanj0/kopfkino/serialization/Serialized.java
@@ -0,0 +1,38 @@
+/*
+ *    Copyright 2021 ***REMOVED*** ***REMOVED***
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ *
+ */
+
+package de.sanj0.kopfkino.serialization;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Serialized {
+
+    /**
+     * The key of the value that this field
+     * is stored in and read from, relative to its
+     * parent's {@link Serializable#id() sanjo class}.
+     *
+     * @return the key of the value associated with this field
+     * @see de.edgelord.sanjo.SJValue
+     */
+    String key() default "";
+}
